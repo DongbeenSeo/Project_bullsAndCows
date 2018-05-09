2d00432d01da9e1342130fe0701436c605f174ce
DongbeenSeo <puus1021@gmail.com>
2018-05-09 17:56:01 +0900

    changed js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
 class baseballGame {
-  answer = [];
-  strike = 1;
-  ball = 0;
+    answer = [];
+    strike = 1;
+    ball = 0;
 
-  randomNumber(n) {
-      let len = n;
-      this.answer = new Array(len);
-      this.answer.fill(0);
+    randomNumber(n) {
+        let len = n;
+        this.answer = new Array(len);
+        this.answer.fill(0);
 
-      for (let i = 0; i < len; i++) {
-          this.answer[i] = Math.floor(Math.random() * 10);
-          for (let j = 0; j < i; j++) {
-              if (this.answer[i] === this.answer[j]) {
-                  i--;
-                  break;
-              }
-          }
-      }
-      return this.answer;
-  }
+        for (let i = 0; i < len; i++) {
+            this.answer[i] = Math.floor(Math.random() * 10);
+            for (let j = 0; j < i; j++) {
+                if (this.answer[i] === this.answer[j]) {
+                    i--;
+                    break;
+                }
+            }
+        }
+        return this.answer;
+    }
+
+    check() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.player[i] === this.answer[j]) {
+                    if (i === j) {
+                        this.strike++;
+                    } else {
+                        this.ball++;
+                    }
+                }
+            }
+        }
+    }
 }
 const game = new baseballGame();
 
