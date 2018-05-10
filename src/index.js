6e190ade36b0a9867165ecf076991b2799179ec4
DongbeenSeo <puus1021@gmail.com>
2018-05-10 17:17:27 +0900

    기본 로직 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 class baseballGame {
-    player= [];
+    player = [];
     answer = [];
     strike = 0;
     ball = 0;
@@ -10,7 +10,7 @@ class baseballGame {
         this.answer.fill(0);
 
         for (let i = 0; i < len; i++) {
-            this.answer[i] = Math.floor(Math.random() * 10);
+            this.answer[i] = Math.floor(Math.random() * 10).toString();
             for (let j = 0; j < i; j++) {
                 if (this.answer[i] === this.answer[j]) {
                     i--;
@@ -20,7 +20,6 @@ class baseballGame {
         }
         return this.answer;
     }
-
     check() {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
@@ -38,15 +37,64 @@ class baseballGame {
 
 const game = new baseballGame();
 
-game.randomNumber(3);
+const inputEls = document.querySelectorAll('.number')
+const trialEl = document.querySelector('.trial');
+const resetEl = document.querySelector('.reset');
+const displayEl = document.querySelector('.display');
 
+const round__countEl = document.querySelector('.round__count');
+const round__displayEls = document.querySelectorAll('.round__display');
+const round__resultEl = document.querySelector('.round__result');
+const final__answerEl = document.querySelector('.final_answer');
 
-const inputNums = document.querySelectorAll('.number');
-const trial = document.querySelector('.trial');
-const reset = document.querySelector('.reset');
+init();
+console.log(game.answer);
 
-console.log(inputNums);
-
-inputNums.addEventListener('input', e => {
-    // forEach()
+inputEls.forEach((el, index) => {
+    el.addEventListener('input', e => {
+        game.player[index] = e.target.value;
+        console.log(game.player);
+    })
 })
+
+trialEl.addEventListener('click', e => {
+    displayEl.style.display = 'contents'
+    if (cnt <= 8) {
+        round__countEl.textContent = `${++cnt}회차`;
+        round__displayEls.forEach((el, index) => {
+            el.textContent = game.player[index];
+        })
+        game.check();
+        round__resultEl.textContent = `${game.strike} S ${game.ball} B`
+        game.strike = 0, game.ball = 0;
+    } else {
+        final__answerEl.textContent = game.answer;
+        document.querySelector('.trial').disabled = true;
+    }
+})
+
+//리셋 버튼 눌렀을 때 
+//게임상태 초기화
+resetEl.addEventListener('click', e => {
+    trialEl.disabled = false;
+    game.player = [];
+    console.log('reset');
+    round__displayEls.forEach((el, index) => {
+        el.textContent = '';
+    })
+    inputEls.forEach((el, index) => {
+        el.value = '';
+    })
+    round__countEl.textContent = '';
+    round__resultEl.textContent = '';
+    final__answerEl.textContent = '';
+    init();
+})
+
+function init() {
+    game.answer = game.randomNumber(3);
+    cnt = 0;
+    game.strike = 0;
+    game.ball = 0;
+    displayEl.style.display = 'none';
+}
